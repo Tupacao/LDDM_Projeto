41f111f21056ee6e8c930425337b367f467ee085
Tupacao <arthur12268@gmail.com>
2024-09-30 19:23:42 -0300

    EventCalendar pronto

--- lib/Pages/EventCalendar.dart
+++ lib/Pages/EventCalendar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:projeto/Components/EventCalendarPop.dart';
+import 'package:projeto/Components/NavBottombar.dart';
+import 'package:projeto/Components/UserIcon.dart';
+import 'package:projeto/assets/Colors.dart';
 
 class EventCalendar extends StatefulWidget {
   const EventCalendar({super.key});
@@ -10,6 +14,59 @@ class EventCalendar extends StatefulWidget {
 class _EventCalendarState extends State<EventCalendar> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const UserIcon(showCross: false),
+      body: Column(
+        children: [
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Agenda de Eventos",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: EventCalendarPop(),
+                );
+              },
+            ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: FilledButton(
+                onPressed: () {
+                  // funcao para deletar
+                },
+                style: FilledButton.styleFrom(
+                    backgroundColor: accentColor,
+                    minimumSize: const Size(100, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+                child: const Text(
+                  "Deletar",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: textColor,
+                  ),
+                ),
+              ),
+            )
+          ])
+        ],
+      ),
+      backgroundColor: secondaryColor,
+      bottomNavigationBar: const NavBottomBar(),
+    );
   }
 }
