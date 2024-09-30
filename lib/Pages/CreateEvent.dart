cb39d9fb2381ee0d8d33bd75589ba664c4d665e2
Tupacao <arthur12268@gmail.com>
2024-09-30 20:22:57 -0300

    Fim do CreateEvent

--- lib/Pages/CreateEvent.dart
+++ lib/Pages/CreateEvent.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:projeto/Components/NavBottombar.dart';
+import 'package:projeto/Components/UserIcon.dart';
+import 'package:projeto/assets/Colors.dart';
 
 class CreateEvent extends StatefulWidget {
   const CreateEvent({super.key});
@@ -10,6 +13,189 @@ class CreateEvent extends StatefulWidget {
 class _CreateEventState extends State<CreateEvent> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const UserIcon(showCross: false),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: const BoxDecoration(
+                    color: primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.no_accounts_outlined,
+                    color: secondaryColor,
+                    size: 100,
+                  ),
+                ),
+                const SizedBox(width: 20),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nome da Pessoa",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "alguma descricao a mais",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Texto",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: null,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: primaryColor,
+                  ),
+                  style: TextStyle(
+                    color: secondaryColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Texto",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: null,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: primaryColor,
+                  ),
+                  style: TextStyle(
+                    color: secondaryColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Texto",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Card(
+                  color: primaryColor,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      maxLines: 5,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Escreva sua descrição aqui...',
+                      ),
+                      style: const TextStyle(
+                        color: secondaryColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                FilledButton(
+                  onPressed: () {
+                    // funcao para salvar
+                  },
+                  style: FilledButton.styleFrom(
+                      backgroundColor: accentColor,
+                      minimumSize: const Size(100, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      )),
+                  child: const Text(
+                    "Salvar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                FilledButton(
+                  onPressed: () {
+                    // funcao para deletar
+                  },
+                  style: FilledButton.styleFrom(
+                      backgroundColor: accentColor,
+                      minimumSize: const Size(100, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      )),
+                  child: const Text(
+                    "Deletar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      backgroundColor: secondaryColor,
+      bottomNavigationBar: const NavBottomBar(),
+    );
   }
 }
