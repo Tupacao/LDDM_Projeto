3873e2859b3db162a3df3cc66d5ac3dd975aeac8
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-10-01 07:52:58 -0300

    home ui change

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -21,54 +21,56 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
               child: Column(
                 children: [
-                  TextField(
-                    controller: _searchController,
-                    keyboardType: TextInputType.name,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: accentColor,
-                      suffixIcon: Icon(
-                        Icons.search,
-                        color: textColor,
-                        size: 30,
+                  SizedBox(
+                    width: 800,
+                    child: TextField(
+                      controller: _searchController,
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        label: Text("Pesquisar evento"),
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: textColor,
+                          size: 30,
+                        ),
                       ),
-                    ),
-                    style: const TextStyle(
-                      color: secondaryColor,
+                      style: const TextStyle(color: textColor),
                     ),
                   ),
                   const SizedBox(height: 10),
-                  Align(
-                    alignment: Alignment
-                        .centerLeft, // Alinha o Container ao início (start)
-                    child: Container(
-                      width: 100,
-                      padding: const EdgeInsets.all(10), // Adiciona padding
-                      decoration: BoxDecoration(
-                        color: accentColor,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Filtro",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
+                  SizedBox(
+                      width: 800,
+                      child: Align(
+                        alignment: Alignment
+                            .centerLeft, // Alinha o Container ao início (start)
+                        child: Container(
+                          width: 100,
+                          padding: const EdgeInsets.all(10), // Adiciona padding
+                          decoration: BoxDecoration(
+                            color: accentColor,
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                          SizedBox(
-                            width: 8, // Espaço entre o texto e o ícone
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Filtro",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 8, // Espaço entre o texto e o ícone
+                              ),
+                              Icon(Icons.add),
+                            ],
                           ),
-                          Icon(Icons.add),
-                        ],
-                      ),
-                    ),
-                  )
+                        ),
+                      )),
                 ],
               )),
           Expanded(
